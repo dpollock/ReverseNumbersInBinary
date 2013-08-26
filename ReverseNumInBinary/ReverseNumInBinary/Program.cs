b04d80b1f16b393fdecc0a24f81e28f11383779e
Daniel Pollock <daniellpollock@gmail.com>
2013-08-26 10:24:52 -0500

    Initial Commit
    Brute force (and slow!) - converts int to base 2 as a string and then
    reverses the string and converts back to int 64

---
+++ ReverseNumInBinary/ReverseNumInBinary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ReverseNumInBinary
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            for (long i = 1; i <= 1000000000; i++)
+            {
+                ulong result = ReverseInBinary(i);
+
+                if (i % 1000000 == 0) //every million spit out a number
+                    Console.WriteLine("{0} -> {1}", i, result);
+
+            }
+
+            watch.Stop();
+            long time = watch.ElapsedMilliseconds;
+            Console.WriteLine("Elapsed Time(ms): {0}", time);
+            Console.ReadLine();
+        }
+
+        public static ulong ReverseInBinary(long num)
+        {
+            string binaryString = Convert.ToString(num, 2);
+            string reversedBinaryString = ReverseString(binaryString);
+            ulong result = Convert.ToUInt64(reversedBinaryString, 2);
+            return result;
+        }
+
+        public static string ReverseString(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
